c4a62c1ee1d489d47c3932d97b668a90154276dd
朱力 <lzhu@192.168.144.1>
2017-08-15 09:50:54 +0800

    Add flipImage

--- Gestures/Gestures/ViewController.swift
+++ Gestures/Gestures/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func flipImage(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
